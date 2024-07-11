540c8e3c9c21a6e1a4a82195d9d2103a564a51e8
broCapang <u2000429@siswa.um.edu.my>
2024-07-11 11:02:30 -0400

    solved Simple Crack Me 2

--- dreamHack/simple_Crack_Me2/solve.py
+++ dreamHack/simple_Crack_Me2/solve.py
@@ -2,23 +2,27 @@ def xor_byte(data, key):
     length = len(key)
     for i in range(0x20):
         data[i] ^= key[i % length]
+        data[i] &= 0xFF
 
 def increase_I(data, value):
     for i in range(0x20):
         data[i] = (data[i] + value) 
+        data[i] &= 0xFF
 
 def decrease_I(data, value):
     for i in range(0x20):
         data[i] = (data[i] - value) 
+        data[i] &= 0xFF
 
 def invert_operations(data, first_bytes, second_bytes, third_bytes):
     xor_byte(data, third_bytes)
-    increase_I(data, -0xF3)  # Equivalent to increase_I with positive value
-    decrease_I(data, -0x4D)  # Equivalent to decrease_I with positive value
+    decrease_I(data, 0xF3)  
+    increase_I(data, 0x4D)  
     xor_byte(data, second_bytes)
-    decrease_I(data, -0x5A)  # Equivalent to decrease_I with positive value
-    increase_I(data, -0x1F)  # Equivalent to increase_I with positive value
+    increase_I(data, 0x5A)  
+    decrease_I(data, 0x1F)  
     xor_byte(data, first_bytes)
+
 
 enc_flag = [ 0xf8, 0xe0, 0xe6, 0x9e, 0x7f, 0x32, 0x68, 0x31, 0x05, 0xdc, 0xa1, 0xaa, 0xaa, 0x09, 0xb3, 0xd8, 0x41, 0xf0, 0x36, 0x8c, 0xce, 0xc7, 0xac, 0x66, 0x91, 0x4c, 0x32, 0xff, 0x05, 0xe0, 0xd9, 0x91]
 
@@ -43,8 +47,12 @@ enc_flag =xor_byte(enc_flag, thirdBytes)
 # Call the function to invert the operations
 invert_operations(enc_flag, firstBytes, secondBytes, thirdBytes)
 
-
 print(enc_flag)
-flag = [chr(value) for value in enc_flag]
-print(flag)
 
+flag = [chr(value) for value in enc_flag]
+print(''.join(flag))
+
+
+'''
+9ce745c0d5faaf29b7aecd1a4a72bc86
+'''
